de006e82d0c0dfed14066f144ea61a12fe4e4a5c
Brithney Salazar <93927947+Brithn@users.noreply.github.com>
2025-03-06 16:40:28 -0500

    Calendario

--- src/components/CalendarView.js
+++ src/components/CalendarView.js
@@ -1,14 +1,20 @@
 import React from "react";
+import "./CalendarView.css";
+import { FaCalendarAlt } from "react-icons/fa";
+
 
 const CalendarView = ({ events }) => {
   return (
     <div className="calendar-container">
-      <h2>Vista de Calendario</h2>
-      <div className="calendar">
+      <h2 className="calendar-title">
+        <FaCalendarAlt className="calendar-icon" />Calendario
+      </h2>
+      <hr></hr>
+      <div className="calendar-grid">
         {events.map((event) => (
           <div className="calendar-event" key={event.id}>
-            <strong>{event.date}</strong>
-            <p>{event.name}</p>
+            <strong className="event-date">{event.date}</strong>
+            <p className="event-name">{event.name}</p>
           </div>
         ))}
       </div>
